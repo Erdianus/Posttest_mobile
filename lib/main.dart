2f1bf1abf9d3fe6730238c4cdbd60cbe2abdb4ea
Erdianus <72244475+Erdianus@users.noreply.github.com>
2022-04-02 12:01:21 +0800

    POSTTEST 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,56 +7,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo000',
+        title: 'Posttest 1',
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
         home: FirstScreen());
-  }
-}
-
-class Heading extends StatelessWidget {
-  final String text;
-
-  Heading({required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-  const BiggerText({required this.text});
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: Text("Perbesar"),
-          onPressed: () {
-            setState(() {
-              _textSize = 32.0;
-            });
-          },
-        )
-      ],
-    );
   }
 }
 
@@ -77,11 +32,12 @@ class FirstScreen extends StatelessWidget {
         title: Text('Posttest 1 Erdianus Pagesong'),
         actions: <Widget>[
           IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
-              ))
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          )
         ],
       ),
       body: Center(
@@ -92,13 +48,6 @@ class FirstScreen extends StatelessWidget {
               end: Alignment.bottomRight,
               colors: [Colors.green, Color.fromARGB(255, 121, 158, 119)],
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black,
-                offset: Offset(3, 6),
-                blurRadius: 10,
-              )
-            ],
           ),
           child: Center(
             child: Text(
